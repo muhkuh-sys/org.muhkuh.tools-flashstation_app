742e62a4fa25a67a1da7c823cb81da7aa456ba64
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2021-05-11 12:48:41 +0200

    Add a function to send messages to a TCP server.

--- src/network_lwip.c
+++ src/network_lwip.c
@@ -391,8 +391,230 @@ int httpDownload(ip4_addr_t *ptServerIpAddr, const char *pcUri, unsigned char *p
 			{
 				memcpy(ptHash, &(tHttpDownload.tHash), sizeof(SHA384_T));
 			}
+
+			iResult = -1;
 		}
 	}
 
+	return iResult;
+}
+
+
+
+typedef struct SEND_MESSAGE_CALLBACK_STRUCT
+{
+	unsigned int uiIsConnected;
+	unsigned long ulSentData;
+	err_t tResult;
+	unsigned long ulChunkTransmitTimeoutInMs;
+	TIMER_HANDLE_T tTimer;
+} SEND_MESSAGE_CALLBACK_T;
+
+
+
+static err_t sendMessageConnectCallback(void *pvUser, struct tcp_pcb *ptPcb __attribute__((unused)), err_t tErr)
+{
+	SEND_MESSAGE_CALLBACK_T *ptUser;
+
+
+//	uprintf("Connect Callback %d\n", tErr);
+	if( tErr==ERR_OK )
+	{
+		ptUser = (SEND_MESSAGE_CALLBACK_T*)pvUser;
+		if( ptUser!=NULL )
+		{
+			ptUser->uiIsConnected = 1U;
+		}
+		else
+		{
+			tErr = ERR_VAL;
+		}
+	}
+
+	return tErr;
+}
+
+
+
+static err_t sendMessageSentCallback(void *pvUser, struct tcp_pcb *ptPcb __attribute__((unused)), u16_t usLen)
+{
+	err_t tResult;
+	SEND_MESSAGE_CALLBACK_T *ptUser;
+
+
+//	uprintf("Sent Callback %d\n", usLen);
+	ptUser = (SEND_MESSAGE_CALLBACK_T*)pvUser;
+	if( ptUser!=NULL )
+	{
+		ptUser->ulSentData += usLen;
+
+		/* Sent something. */
+		systime_handle_start_ms(&(ptUser->tTimer), ptUser->ulChunkTransmitTimeoutInMs);
+
+		tResult = ERR_OK;
+	}
+	else
+	{
+		tResult = ERR_VAL;
+	}
+
 	return tResult;
+}
+
+
+
+int sendMessage(ip4_addr_t *ptServerIpAddr, unsigned short usServerPort, const void *pvMessage, unsigned int sizMessage)
+{
+	int iResult;
+	err_t tResult;
+	struct tcp_pcb *ptPcb;
+	const unsigned char *pucCnt;
+	const unsigned char *pucEnd;
+	unsigned int sizLeft;
+	u16_t usChunk;
+	u8_t ucFlags;
+	SEND_MESSAGE_CALLBACK_T tCallbackData;
+	const unsigned long ulConnectionTimeoutInMs = 5000U;
+	const unsigned long ulChunkTransmitTimeoutInMs = 10000U;
+
+
+	iResult = -1;
+
+	tCallbackData.uiIsConnected = 0;
+	tCallbackData.ulSentData = 0;
+	tCallbackData.tResult = ERR_OK;
+
+	/* Create a new PCB for the connection. */
+	ptPcb = tcp_new();
+	if( ptPcb==NULL )
+	{
+		uprintf("Failed to allocate a new pcb.\n");
+	}
+	else
+	{
+		/* Add the callback data to the PCB. */
+		tcp_arg(ptPcb, &tCallbackData);
+
+		/* Set a callback for transmitted data. */
+		tcp_sent(ptPcb, sendMessageSentCallback);
+
+		/* Connect. */
+		tResult = tcp_connect(ptPcb, ptServerIpAddr, usServerPort, sendMessageConnectCallback);
+		if( tResult!=ERR_OK )
+		{
+			uprintf("Failed to connect.\n");
+		}
+		else
+		{
+			/* Wait for the connection. */
+			systime_handle_start_ms(&(tCallbackData.tTimer), ulConnectionTimeoutInMs);
+			while( tCallbackData.tResult==ERR_OK && tCallbackData.uiIsConnected==0U )
+			{
+				network_cyclic_process();
+				if( systime_handle_is_elapsed(&(tCallbackData.tTimer))!=0 )
+				{
+					tResult = ERR_TIMEOUT;
+					break;
+				}
+			}
+//			uprintf("%d: Connected: %d %d %d\n", systime_get_ms(), tResult, tCallbackData.tResult, tCallbackData.uiIsConnected);
+
+			if( tResult==ERR_OK && tCallbackData.tResult==ERR_OK )
+			{
+				systime_handle_start_ms(&(tCallbackData.tTimer), ulChunkTransmitTimeoutInMs);
+				tCallbackData.ulChunkTransmitTimeoutInMs = ulChunkTransmitTimeoutInMs;
+				pucCnt = (const unsigned char*)pvMessage;
+				pucEnd = pucCnt + sizMessage;
+				while( pucCnt<pucEnd )
+				{
+					sizLeft = (unsigned int)(pucEnd-pucCnt);
+
+					/* How many bytes can be send now?
+					 * This is limited by the available space in the stack, which can be queried with tcp_sndbuf, and
+					 * the amount of unsent bytes left in the buffer.
+					 *
+					 * This part also sets the flags for the write operation.
+					 * If the remaining data in the buffer does not fit into the next send chunk,
+					 * set "TCP_WRITE_FLAG_MORE" to indicate that there will be more data.
+					 */
+					ucFlags = TCP_WRITE_FLAG_MORE;
+					usChunk = tcp_sndbuf(ptPcb);
+					if( usChunk>0 )
+					{
+						if( usChunk>=sizLeft )
+						{
+							usChunk = (u16_t)sizLeft;
+							ucFlags = 0U;
+						}
+
+						/* Queue the data. */
+						tResult = tcp_write(ptPcb, pucCnt, usChunk, ucFlags);
+						if( tResult!=ERR_OK )
+						{
+							uprintf("Failed to queue data: %d\n", tResult);
+							break;
+						}
+
+						/* Move the buffer pointer to the next unsent data. */
+						pucCnt += usChunk;
+					}
+
+					/* Check for a send timeout. */
+					if( systime_handle_is_elapsed(&(tCallbackData.tTimer))!=0 )
+					{
+						uprintf("Send timeout!\n");
+						tResult = ERR_TIMEOUT;
+						break;
+					}
+
+					network_cyclic_process();
+				}
+				if( tResult==ERR_OK )
+				{
+					tResult = tcp_output(ptPcb);
+					if( tResult!=ERR_OK )
+					{
+						uprintf("Pusing the data with tcp_output failed: %d\n", tResult);
+					}
+					else
+					{
+						/* Wait until all data is sent. */
+						while( tCallbackData.ulSentData<sizMessage )
+						{
+							/* Check for a send timeout. */
+							if( systime_handle_is_elapsed(&(tCallbackData.tTimer))!=0 )
+							{
+								uprintf("Push timeout!\n");
+								tResult = ERR_TIMEOUT;
+								break;
+							}
+
+							network_cyclic_process();
+						}
+					}
+				}
+			}
+
+			if( tResult==ERR_OK )
+			{
+				tResult = tcp_close(ptPcb);
+				if( tResult!=ERR_OK )
+				{
+					uprintf("Close failed: %d\n", tResult);
+				}
+			}
+
+			if( tResult==ERR_OK )
+			{
+				/* All ok. */
+				iResult = 0;
+			}
+			else
+			{
+				tcp_abort(ptPcb);
+			}
+		}
+	}
+
+	return iResult;
 }
